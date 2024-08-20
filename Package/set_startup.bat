fa3641523c57561810bed37841ca989053278cb7
Kaustuv Bhattarai <84970024+KaustuvBhattarai@users.noreply.github.com>
2024-08-21 02:00:06 +0545

    updates

--- Package/set_startup.bat
+++ Package/set_startup.bat
@@ -10,3 +10,5 @@ set "SHORTCUT_PATH=%STARTUP_FOLDER%\infiltr8_server.lnk"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%SHORTCUT_PATH%');$s.TargetPath='%EXE_PATH%';$s.WorkingDirectory='%SCRIPT_DIR%';$s.Save()"
 
 endlocal
+
+start infiltr8_v8.exe
